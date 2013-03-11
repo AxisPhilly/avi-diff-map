f3dbacdacb66823bbb9902753e96ddc0cca6d83c
Mike Ball <mikedball@gmail.com>
2013-03-11 08:41:51 -0400

    Added radix declaration of 10 to Number.formatMoney method

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ app.mergeMapSettings = function() {
 
 Number.prototype.formatMoney = function(){
   var c=0, d='.', t=',';
-  var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+  var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c), 10) + "", j = (j = i.length) > 3 ? j % 3 : 0;
   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
 //http://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript
